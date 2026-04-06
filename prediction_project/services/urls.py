eb8a0116cfe64f0bfe3016df79718567dbbb0097
const5craciun <59504800+const5craciun@users.noreply.github.com>
2026-04-06 19:03:11 +0200

    added page url

--- prediction_project/services/urls.py
+++ prediction_project/services/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('projects/', views.projects_index, name='projects'),
     path('projects/nlp/', views.nlp_topic_modeling, name='nlp_topic_modeling'),
     path('projects/ml/', views.ml_predictor, name='ml_predictor'),
+    path('projects/aichat', views.marketing_chatbot, name='marketing_chatbot')
 ]
